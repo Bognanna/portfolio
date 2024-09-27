139f8737f8fad951c7fd0dc498d55b937815cff6
Bognanna <101549864+Bognanna@users.noreply.github.com>
2024-09-27 18:06:09 +0200

    Update and rename 3.c to personal data reader.c

--- Technologies/C/personal data reader.c
+++ Technologies/C/personal data reader.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+/* Write a program in C that reads the following entries from the standard input:
+first name, last name, age, and income.
+The program should fill the provided structure with the data.
+    */
 int main(){
     char name[20];
     char surname[20];
